9dafa4048b0cf4db2428fca9239ebdcd5960306c
Muhammad Imran <muhammadi1@bradfordcollege.ac.uk>
2018-02-01 11:50:32 +0000

    PieDetailController chngs

--- ClientApp/src/app/pie-detail/pie-detail.component.ts
+++ ClientApp/src/app/pie-detail/pie-detail.component.ts
@@ -10,33 +10,28 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
   styleUrls: ['./pie-detail.component.css']
 })
 
-export class PieDetailComponent implements OnInit {
+export class PieDetailComponent  {
 
   public pie: Pie;
+  
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string , private activatedRoute: ActivatedRoute) {
 
-  constructor(@Inject('BASE_URL') baseUrl: string ,private http: HttpClient, private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    // subscribe to router event
     this.activatedRoute.params.subscribe((params: Params) => {
-        let Id = params['id'];
+      let Id = params['id'];
+      console.log(Id);   
+      http.get<Pie>(baseUrl + 'api/PieData/GetPieDetail'+Id).subscribe(result => {
+        this.pie = result;
+        //console.log(baseUrl + 'api/PieData/GetAllPies');
+      }, error => console.error(error));
 
-         this.http.get<Pie>('api/PieData/GetPieDetail/'+Id).subscribe(result => {
-         this.pie = result;
-
-         console.log(Id);
-      });
+    });
   }
   
-    
-  // constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-  //   http.get<Pie>(baseUrl + 'api/PieData/GetPieDetail/${id}').subscribe(result => {
-  //     this.pie = result;
-      
-  //   }, error => console.error(error));
-  // }
-
 }
+  
+    
+
+
 
 interface Pie {
   Id: number;
--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -37,12 +37,12 @@ namespace my_new_app.Controllers
            return pies;
        
         }
-
+           [HttpGet("PieData/{id}", Name = "PieDetial")]
          public Pie GetPieDetail(int id)
          {
-             var pie = new Pie();
+             var pie =  new Pie() { Name = "Apple Pie", Price = 12.95M, ShortDescription = "Our famous apple pies!", LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.", ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepie.jpg", IsPieOfTheWeek = true, ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg" };
              
-             pie = _pieRepository.GetPieById(id);
+             //pie = _pieRepository.GetPieById(id);
                               
              return pie;
              
