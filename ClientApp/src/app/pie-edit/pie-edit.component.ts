b5bf4aa7a5f889459fcff9ebdc38c13820da4b60
imran <haidry786@hotmail.com>
2018-02-04 23:05:21 +0000

    Changes

--- ClientApp/src/app/pie-edit/pie-edit.component.ts
+++ ClientApp/src/app/pie-edit/pie-edit.component.ts
@@ -13,22 +13,7 @@ import {PieService} from '../dataServices/pie.service';
 export class PieEditComponent {
 
   public pie: Pie;
-
-  powers = ['Really Smart', 'Super Flexible', 'Weather Changer'];
-
-  hero = {name: 'Dr.', alterEgo: 'Dr. What', power: this.powers[0]};
-
-  pie1 = { id: 1, name: "Apple Pie", 
-            shortDescription: "Our famous apple pies!", 
-            longDescription: "Icing carrot cake jelly-o cheesecake. Sweet roll m…nish lemon drops. Brownie cupcake dragée gummies.", 
-            price: 12.95,
-            imageThumbnailUrl:"https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg",
-            imageUrl: "https://gillcleerenpluralsight.blob.core.windows.net/files/applepie.jpg" ,
-            isInStock:false , isPieOfTheWeek:true
-
-          }
-
-          
+         
   constructor(http: HttpClient ,private activatedRoute: ActivatedRoute, private pieService: PieService) { }
 
   ngOnInit() {
@@ -43,7 +28,7 @@ export class PieEditComponent {
   getPie(id:number) : void {
     this.pieService.getPie(id).subscribe(result => {
       this.pie = result;
-      console.log(this.pie);
+     // console.log(this.pie);
     }, error => console.error(error));
   }
 
