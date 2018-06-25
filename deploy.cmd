47f8a3ceb1c9951c5a770f0dc1ace61f3f2cc07c
imran <haidry786@hotmail.com>
2018-06-25 02:59:16 +0100

    .

--- deploy.cmd
+++ deploy.cmd
@@ -38,47 +38,39 @@ IF NOT DEFINED NEXT_MANIFEST_PATH (
   )
 )
 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Deployment
-:: ----------
- 
-:Deployment
-echo Handling node.js deployment.
- 
-:: 1. Select node version
-call :SelectNodeVersion
- 
-:: 2. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd !NPM_CMD! install
+:: Installing NPM dependencies.
+echo =======  Installing npm  devDependancy packages: Starting at %TIME% ======= 
+echo "%DEPLOYMENT_SOURCE%\ClientApp\package.json"
+IF EXIST "%DEPLOYMENT_SOURCE%\ClientApp\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%\ClientApp"
+  call npm install --save
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
- 
-:: 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
-echo Building App in %DEPLOYMENT_SOURCE%…
-pushd "%DEPLOYMENT_SOURCE%"
-call :ExecuteCmd !NPM_CMD! run build
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
+echo =======  Installing npm dev packages: Finished at %TIME% =======
+
+:: Building the Angular App
+echo =======  Building Angular App: Starting at %TIME% ======= 
+echo "%DEPLOYMENT_SOURCE%\ClientApp\.angular-cli.json"
+IF EXIST "%DEPLOYMENT_SOURCE%\ClientApp\.angular-cli.json" (
+  pushd "%DEPLOYMENT_SOURCE%\ClientApp"
+  call :ExecuteCmd node_modules\.bin\ng build --progress false --prod
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
- 
-:: 4. Copy Web.config
+echo =======  Building Angular App: Finished at %TIME% =======
+
+:: Copy Web.Config
+echo =======  Copy Web.Config: Starting at %TIME% ======= 
+echo "%DEPLOYMENT_SOURCE%\web.config"
 IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
   pushd "%DEPLOYMENT_SOURCE%"
- :: the next line is optional to fix 404 error see section #8
   call :ExecuteCmd cp web.config dist\
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
- 
-:: 5. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
+echo =======  Copy Web.Config: Finished at %TIME% =======
+
 
 IF NOT DEFINED KUDU_SYNC_CMD (
   :: Install kudu sync
