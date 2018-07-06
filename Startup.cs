7b4a254bed01696b4063b91e3e3651c972eeaefd
Muhammad Imran <haidery786@hotmail.com>
2018-07-06 09:49:27 +0100

    .

--- Startup.cs
+++ Startup.cs
@@ -134,23 +134,23 @@ namespace my_new_app
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseExceptionHandler(
-                builder =>
-                {
-                    builder.Run(
-                            async context =>
-                            {
-                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                            context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            // app.UseExceptionHandler(
+            //     builder =>
+            //     {
+            //         builder.Run(
+            //                 async context =>
+            //                 {
+            //                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            //                 context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
 
-                            var error = context.Features.Get<IExceptionHandlerFeature>();
-                            if (error != null)
-                            {
-                            context.Response.AddApplicationError(error.Error.Message);
-                            await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
-                            }
-                        });
-                });
+            //                 var error = context.Features.Get<IExceptionHandlerFeature>();
+            //                 if (error != null)
+            //                 {
+            //                 context.Response.AddApplicationError(error.Error.Message);
+            //                 await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+            //                 }
+            //             });
+            //     });
 
          
             app.UseAuthentication();
